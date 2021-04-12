eb818b443e8a8b853c54ded38da3a2d41c6f6658
swaroopc236 <swaroopc236@gmail.com>
2021-04-13 02:15:08 +0530

    server, routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const itemRouter = require('./Items/ItemRoutes');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.status(200).json({
-        "msg": "Welcome to the best E-Commerce site in the universe."
-    })
-})
+	res.status(200).json({
+		msg: 'Welcome to the best E-Commerce site in the universe.',
+	});
+});
+
+app.use('/items', itemRouter);
+
 app.listen(5000, () => {
 	console.log('Backend serving on port 5000...');
 });
